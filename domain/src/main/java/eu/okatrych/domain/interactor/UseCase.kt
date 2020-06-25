6e6be210a42b63c06b3ec0f5aab32895bedf74f9
Oleksandr Katrych <oleksandr.katrych@sigma.se>
2020-06-25 19:27:25 +0200

    Params has to be not null
    Signed-off-by: Oleksandr Katrych <oleksandr.katrych@sigma.se>

--- domain/src/main/java/eu/okatrych/domain/interactor/UseCase.kt
+++ domain/src/main/java/eu/okatrych/domain/interactor/UseCase.kt
@@ -3,6 +3,11 @@ package eu.okatrych.domain.interactor
 import kotlinx.coroutines.*
 
 /**
+ * Is used to declare that there is no parameters for UseCase
+ */
+typealias None = Unit
+
+/**
  * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
  * This abstraction represents an execution unit for different use cases (this means than any use
  * case in the application should implement this contract).
@@ -14,29 +19,11 @@ abstract class UseCase<in Params, out Type>(
     private val executionDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
 
-    protected abstract suspend fun run(params: Params?): Type
+    protected abstract suspend fun run(params: Params): Type
 
-    suspend operator fun invoke(params: Params? = null, job: Job): Type {
+    suspend operator fun invoke(params: Params, job: Job): Type {
         return withContext(job + executionDispatcher) {
             run(params)
         }
     }
-
-    /**
-     * Used to declare that there is no [Params] for UseCase
-     *
-     * For example:
-     *     <code>
-     *     class SampleUseCase: UseCase<None, String> {
-     *         suspend fun run(params: None?): String {
-     *             ...
-     *         }
-     *     }
-     *
-     *     fun test(sampleUseCase: SampleUseCase) {
-     *         sampleUseCase(job = Job())
-     *     }
-     *     </code>
-     */
-    class None private constructor()
 }
